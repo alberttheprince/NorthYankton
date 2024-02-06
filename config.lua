0f94ddff25867d2610367f460a9e2db4c60d3047
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2024-02-06 15:55:52 -0500

    clean up config

--- config.lua
+++ config.lua
@@ -1,71 +1,30 @@
-PL = {}
+Config = {}
 
------------------------------------------------------------------------------------------------------------------------------------------------
------------------------------------------------------------------------------------------------------------------------------------------------
---Misc-----------------------------------------------------------------------------------------------------------------------------------------
+-- General
 
---These values are in seconds.
+Config.DisplayTextDistance = 3        -- Distance (in meters) that players can see teleporter
+Config.UseTeleportDistance = 1        -- Distance (in meters) that players can use teleporter
+Config.FreezePlayerOnTeleport = false -- If players should be frozen during teleporting
+Config.TeleportInVehicle = false      -- If players can teleport while being inside a vehicle
+Config.Keybind = 38                   -- Key to use to teleport. ("E") https://docs.fivem.net/docs/game-references/controls/
 
-PL.LoopTimer = 1                                --Script update time to display text.
 
-PL.WaitTimer = 0.5                              --Timer between the blackscreen.
-PL.ScreenFadeOutTimer = 1                       --How long it should take for screen to fade out.
-PL.ScreenFadeInTimer = 1                        --How long it should take for screen to fade in.
+-- Teleporter locations
 
------------------------------------------------------------------------------------------------------------------------------------------------
------------------------------------------------------------------------------------------------------------------------------------------------
---General--------------------------------------------------------------------------------------------------------------------------------------
- 
-PL.TeleportInVehicle = false                    --If players can teleport while being inside a vehicle.
-PL.DisplayText = "DrawText"                     --Which export used to display text. Options: "DrawText" | "j-textui" | "cd_drawtextui"
-PL.DisplayTextDistance = 3                      --Distance from when people are able to see the text tooltip.
-PL.UseTeleportDistance = 1                     --Distance from when people are able to use the teleporter.
-PL.FreezePlayerOnTeleport = false               --If players should be freezed during the teleport.
- 
------------------------------------------------------------------------------------------------------------------------------------------------
------------------------------------------------------------------------------------------------------------------------------------------------
---Blips----------------------------------------------------------------------------------------------------------------------------------------
-
-PL.UseBlips = false
-
-PL.BlipSprite = 304                             --Set the global blip sprite. Full list: https://docs.fivem.net/docs/game-references/blips/
-PL.BlipColor = 48                               --Set the global blip color. Full list: https://docs.fivem.net/docs/game-references/blips/
-PL.BlipScale = 0.7                              --Set the global blip scale.
- 
------------------------------------------------------------------------------------------------------------------------------------------------
------------------------------------------------------------------------------------------------------------------------------------------------
---Keybinds-------------------------------------------------------------------------------------------------------------------------------------
- 
-PL.Keybind = 38                                 --Key to use to teleport. Current keybind is "E", full list: https://docs.fivem.net/docs/game-references/controls/
- 
------------------------------------------------------------------------------------------------------------------------------------------------
------------------------------------------------------------------------------------------------------------------------------------------------
---Locations------------------------------------------------------------------------------------------------------------------------------------
- 
-PL.Locations = {
+Config.Locations = {
     [1] = {
         location = {
             from = vector3(-1045.21, -2750.24, 21.36),    -- Location inside of LS Airport Double Doors
-            to = vector3(3149.21, -4839.99, 111.89),      -- Teleport location in front of Caipira Airways
-            
-            showBlip = false,                            --Show blip on from-marker.
-            blipText = "North Yankton Departure",              --Blip text on the blip, if showBlip is true.
-            
-            text = "Book Ticket with Yankton Air",                       --Text to display on the from-marker.
-            textColor = "White",                          --Text Color. ONLY APPLIES IF EXPORT IS "DrawText". Options: "White", "Red", "Blue", "Green", "Yellow", "Purple", "Black", "Orange"
+            to = vector3(3149.21, -4839.99, 111.89),      -- Teleport location in front of Caipira Airways            
+            text = "Book Ticket with Yankton Air",        -- Text to display on the teleporter in Los Santos
         },
     },
- 
+
     [2] = {
         location = {
-            from = vector3(3149.21, -4839.98, 111.89), -- Teleport location in front of Caipira Airways
-            to = vector3(-1034.73, -2733.54, 20.17), -- Location at Taxi pick up area outside of LS Airport
-           
-            showBlip = false,
-            blipText = "Book Ticket with Caipira Airways",
-            
-            text = "Book Ticket with Caipira Airways", 
-            textColor = "White",
+            from = vector3(3149.21, -4839.98, 111.89),    -- Teleport location in front of Caipira Airways
+            to = vector3(-1034.73, -2733.54, 20.17),      -- Location at Taxi pick up area outside of LS Airport
+            text = "Book Ticket with Caipira Airways",    -- Text to display on the teleporter in North Yankton
         },
     },
 }
